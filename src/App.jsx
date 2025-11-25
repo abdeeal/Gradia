d714841cfbe744ef548b086f93a3bfb820bc4127
14madina <ssitimadinahalimsiregar@gmail.com>
2025-11-25 08:36:22 +0700

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import { useEffect, useState } from "react";
 
 function App() {
   const { pathname } = useLocation();
-  sessionStorage.setItem("id_workspace", "1");
   const isAuthPage = pathname.startsWith("/auth") || pathname.startsWith("/workspace");
   const isMobile = useMediaQuery({ maxWidth: 767 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
